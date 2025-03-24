cdf6b493c7fa9c0665dd0a3bbe2e41a4518f0963
Leticia da Luz <leticiadluz@gmail.com>
2025-03-24 13:14:25 -0300

    Atualizando readme

--- criacao_tabelas/fact_conversions.sql
+++ criacao_tabelas/fact_conversions.sql
@@ -2,7 +2,7 @@ CREATE TABLE fact_conversions (
     customer_id INT PRIMARY KEY,  
     test_group INT CHECK (test_group IN (0,1)) NOT NULL,  
     converted INT CHECK (converted IN (0,1)) NOT NULL,  
-    communication VARCHAR(20), -- Definir um tamanho adequado para a string
+    communication VARCHAR(20), 
     CONSTRAINT fk_conversions_clients FOREIGN KEY (customer_id) 
         REFERENCES dim_clients(customer_id)
 );
